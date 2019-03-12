be942b547702e99e5c1763efb0a8ec7d5a2049df
Brett Choquet <brett.choquet@gmail.com>
2019-03-12 14:02:40 +0100

    security update on channel connection + removed duplication for message rendering

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,7 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      ActionCable.server.broadcast 'room_channel',
-        content: message.content,
-        username: message.user.username
+      ActionCable.server.broadcast 'room_channel', message: render_message(message)
     else
         Rails.logger.info "Errors: #{message.errors.full_messages}"
     end
@@ -25,5 +23,9 @@ class MessagesController < ApplicationController
 
     def message_params
       params.require(:message).permit(:content)
+    end
+
+    def render_message message
+      render(partial: message, locals: {message: message})
     end
 end
